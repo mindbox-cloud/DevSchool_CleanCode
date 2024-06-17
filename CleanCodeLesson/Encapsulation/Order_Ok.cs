b02c75561160c593095ecaef13044cc971c0e6d9
AlexKalnitskiy <kalnitskiy@mindbox.cloud>
2024-06-17 21:54:39 +0300

    init

---
+++ CleanCodeLesson/Encapsulation/Order_Ok.cs
@@ -0,0 +1,32 @@
+﻿namespace CleanCodeLesson.Encapsulation.GoodOrder;
+
+public class Order
+{
+    public Guid Id { get; set; }
+
+    public bool IsProcessed { get; set; }
+    
+    public int TotalPrice { get; set; }
+    
+    public IList<OrderLine> OrderLines { get; set; }
+}
+
+public class OrderLine
+{
+    public Guid Id { get; set; }
+    
+    public int Price { get; set; }
+}
+
+public interface IOrderCalculator
+{
+    public void CalculateTotalPrice(Order order);
+}
+
+public class OrderCalculationService : IOrderCalculator
+{
+    public void CalculateTotalPrice(Order order)
+    {
+        order.TotalPrice = order.OrderLines.Sum(z => z.Price);
+    }
+}
