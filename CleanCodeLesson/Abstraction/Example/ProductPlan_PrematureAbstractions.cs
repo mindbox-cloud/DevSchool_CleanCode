5a36abf7fe3cb7520243b740a4aa9b350ef0714c
AlexKalnitskiy <kalnitskiy@mindbox.cloud>
2024-06-22 22:29:33 +0300

    product plan premature abstractions

---
+++ CleanCodeLesson/Abstraction/Example/ProductPlan_PrematureAbstractions.cs
@@ -0,0 +1,28 @@
+namespace CleanCodeLesson.Abstraction;
+
+//Задача: тарифы и услуги
+
+//1. Нужно иметь продуктовые планы, где будут наборы услуг с ежедневной тарификацией тарификацией
+//2. Нужно уметь считать стоимость тарифа за период обслуживания
+
+public class ProductPlan
+{
+    public string Name { get; }
+
+    public List<Service> Services { get; private set; }
+
+    public decimal CalculateForPeriod(DateTime from, DateTime to)
+    {
+        var days = (to - from).Days;
+        var sum = Services.Sum(z => z.DaylyCost) * days;
+
+        return sum;
+    }
+}
+
+public class Service
+{
+    public string Name { get; private set; }
+
+    public decimal DaylyCost { get; private set; }
+}
