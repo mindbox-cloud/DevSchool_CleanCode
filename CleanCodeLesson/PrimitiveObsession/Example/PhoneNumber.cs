ca411bf7b42c5461d12a45f4f4f90e6a749a2f88
AlexKalnitskiy <kalnitskiy@mindbox.cloud>
2024-06-22 17:19:20 +0300

    *

--- CleanCodeLesson/PrimitiveObsession/Example/PhoneNumber.cs
+++ CleanCodeLesson/PrimitiveObsession/Example/PhoneNumber.cs
@@ -10,31 +10,6 @@ public sealed record PhoneNumber
     public static PhoneNumber Parse(string value) => new(value.TryParsePhone().ToString() ?? throw new ArgumentException(value, nameof(value)));
 }
 
-public abstract record CustomerPhoneType;
-public sealed record NoPhone : CustomerPhoneType;
-public sealed record AwaitingConfirmation(PhoneNumber PhoneNumber, ConfirmationCode ConfirmationCode) : CustomerPhoneType;
-public sealed record Confirmed(PhoneNumber PhoneNumber) : CustomerPhoneType;
-public sealed record AwaitingRebinding(PhoneNumber CurrentNumber, PhoneNumber PhoneNumber, ConfirmationCode ConfirmationCode) : CustomerPhoneType;
-
-public static class CustomerPhone
-{
-    public static CustomerPhoneType ChangePhone(this CustomerPhoneType current, PhoneNumber newPhone) => current switch
-    {
-        NoPhone => new AwaitingConfirmation(newPhone, ConfirmationCode.Generate(6)),
-        Confirmed confirmed => new AwaitingRebinding(confirmed.PhoneNumber, newPhone, ConfirmationCode.Generate(6)),
-
-        _ => current
-    };
-
-    public static CustomerPhoneType Confirm(this CustomerPhoneType current, ConfirmationCode userInput) => current switch
-    {
-        AwaitingConfirmation awaitingConfirmation => awaitingConfirmation.ConfirmationCode == userInput ? new Confirmed(awaitingConfirmation.PhoneNumber) : throw new InvalidOperationException("User input does not match confirmationCode"),
-        AwaitingRebinding awaitingRebinding => awaitingRebinding.ConfirmationCode == userInput ? new Confirmed(awaitingRebinding.PhoneNumber) : throw new InvalidOperationException("User input does not match confirmationCode"),
-
-        _ => current
-    };
-}
-
 public sealed record ConfirmedPhone(PhoneNumber Phone);
 
 public sealed record AwaitingConfirmationPhone(PhoneNumber PhoneNumber, ConfirmationCode ConfirmationCode)
