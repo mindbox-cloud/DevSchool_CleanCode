9dd53bc1ee8d8cdb654c39c18dffab0aa1308056
aralbaeva <aralbaeva@mindbox.cloud>
2024-07-01 19:12:27 +0300

    add calculate func and test

--- CleanCodeLesson/Homework/MagicWand.cs
+++ CleanCodeLesson/Homework/MagicWand.cs
@@ -4,13 +4,13 @@ namespace CleanCodeLesson.Homework;
 
 public record Length
 {
-    private int _lowerBoundCentimetreIncluded = 18;
-    private int _upperBoundCentimetreIncluded = 50;
+    private const int _lowerBoundCentimetreIncluded = 18;
+    private const int _upperBoundCentimetreIncluded = 50;
     public int Value { get; }
 
     public Length(int length)
     {
-        if (length < _lowerBoundCentimetreIncluded || length > _upperBoundCentimetreIncluded)
+        if (length is < _lowerBoundCentimetreIncluded or > _upperBoundCentimetreIncluded)
         {
             throw new ValidationException(
                 $"Length should be between {_lowerBoundCentimetreIncluded} and {_upperBoundCentimetreIncluded}");
@@ -20,15 +20,15 @@ public record Length
     }
 }
 
-public record FlexabilityСoefficient
+public record FlexibilityСoefficient
 {
-    private double _lowerBoundIncluded = 0.01;
-    private double _upperBoundIncluded = 0.2;
+    private const double _lowerBoundIncluded = 0.01;
+    private const double _upperBoundIncluded = 0.2;
     public double Value { get; }
 
-    public FlexabilityСoefficient(double coefficient)
+    public FlexibilityСoefficient(double coefficient)
     {
-        if (coefficient < _lowerBoundIncluded || coefficient > _upperBoundIncluded)
+        if (coefficient is < _lowerBoundIncluded or > _upperBoundIncluded)
         {
             throw new ValidationException(
                 $"Coefficient should be between {_lowerBoundIncluded} and {_upperBoundIncluded}");
@@ -52,11 +52,11 @@ public record Price
 
 public record Wood
 {
-    public static Wood Taxus => new Wood(0.2m); // Тис
-    public static Wood Holy => new Wood(0.2m); // Остролист
-    public static Wood Cedrus => new Wood(0.3m); // Кедр
-    public static Wood Oak => new Wood(0.4m); // Дуб
-    
+    public static Wood Taxus => new(0.2m); // Тис
+    public static Wood Holy => new(0.2m); // Остролист
+    public static Wood Cedrus => new(0.3m); // Кедр
+    public static Wood Oak => new(0.4m); // Дуб
+
     private Wood(decimal price)
     {
         Price = new Price(price);
@@ -70,7 +70,7 @@ public record Core
     public static Core DragonHeartstring => new Core(2m);
     public static Core UnicornHorn => new Core(1.6m);
     public static Core PhoenixFeather => new Core(4m);
-    
+
     private Core(decimal price)
     {
         Price = new Price(price);
@@ -79,19 +79,23 @@ public record Core
     public Price Price { get; }
 }
 
-
 public record MagicWand
 {
-    public MagicWand(Length length, FlexabilityСoefficient flexabilityСoefficient, Wood wood, Core core)
+    public MagicWand(Length length, FlexibilityСoefficient flexibilityСoefficient, Wood wood, Core core)
     {
         Length = length;
-        FlexabilityСoefficient = flexabilityСoefficient;
+        FlexibilityСoefficient = flexibilityСoefficient;
         Wood = wood;
         Core = core;
     }
 
     public Length Length { get; }
-    public FlexabilityСoefficient FlexabilityСoefficient { get; }
+    public FlexibilityСoefficient FlexibilityСoefficient { get; }
     public Wood Wood { get; }
     public Core Core { get; }
+
+    public decimal CalculatePrice()
+    {
+        return Wood.Price.Value + Core.Price.Value;
+    }
 }
