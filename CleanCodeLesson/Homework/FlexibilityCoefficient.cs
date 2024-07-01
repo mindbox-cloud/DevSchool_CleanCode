c58a49d6f370c0c05b8f3fecaf88f5b0ba3bf618
Ekaterina Grishina <grishina@mindbox.cloud>
2024-07-01 19:23:39 +0200

    *

--- CleanCodeLesson/Homework/FlexibilityCoefficient.cs
+++ CleanCodeLesson/Homework/FlexibilityCoefficient.cs
@@ -2,10 +2,13 @@ namespace CleanCodeLesson.Homework;
 
 public record FlexibilityCoefficient
 {
-    public double Value { get; init; }
+    public double Value { get; }
+
     public FlexibilityCoefficient(double value)
     {
-        if (value is < 0.01 or > 0.2) throw new ArgumentException("Flexibility coefficient must be between 0.01 and 0.2");
+        if (value is < 0.01 or > 0.2)
+            throw new ArgumentOutOfRangeException(nameof(value), "Flexibility coefficient must be between 0.01 and 0.2");
+
         Value = value;
     }
 }
--- CleanCodeLesson/Homework/Length.cs
+++ CleanCodeLesson/Homework/Length.cs
@@ -2,10 +2,13 @@ namespace CleanCodeLesson.Homework;
 
 public record Length
 {
-    public int Value { get; init; }
+    public int Value { get; }
+
     public Length(int value)
     {
-        if (value is < 18 or > 50) throw new ArgumentException("Length must be between 18 and 50");
+        if (value is < 18 or > 50)
+            throw new ArgumentOutOfRangeException(nameof(value), "Length must be between 18 and 50");
+
         Value = value;
     }
 }
