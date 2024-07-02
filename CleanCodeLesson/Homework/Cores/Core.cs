de5b6da5a92e243ac0538fca609cf194650beeb8
Ekaterina Grishina <grishina@mindbox.cloud>
2024-07-02 18:12:11 +0200

    *

--- CleanCodeLesson/Homework/Cores/Core.cs
+++ CleanCodeLesson/Homework/Cores/Core.cs
@@ -1,6 +1,3 @@
 namespace CleanCodeLesson.Homework;
 
-public abstract record Core(Price Price)
-{
-    public virtual Price Price { get; init; } = Price;
-}
+public abstract record Core(Price Price);
--- CleanCodeLesson/Homework/Cores/DragonHeartstring.cs
+++ CleanCodeLesson/Homework/Cores/DragonHeartstring.cs
@@ -1,6 +1,3 @@
 namespace CleanCodeLesson.Homework;
 
-public record DragonHeartstring(DragonType DragonType) : Core(new Price(2m))
-{
-    public override Price Price => 2m * DragonType.PriceMultiplier;
-}
+public record DragonHeartstring(DragonType DragonType) : Core(new Price(2m * DragonType.PriceMultiplier));
